ff757cb9934915a3678525d852bd080da44b3d1e
Agoldenfish030 <rain20854@gmail.com>
2026-06-04 20:36:52 +0800

    TicketsInfo add get function

--- java_final/YourTicketIsMyTicket/src/main/java/app/ticketData/TicketsInfo.java
+++ java_final/YourTicketIsMyTicket/src/main/java/app/ticketData/TicketsInfo.java
@@ -13,17 +13,11 @@ public class TicketsInfo {
         this.ticketStatus = ticketStatus;
     }
 
-    public void setTicketType(String ticketType) {
-        this.ticketType = ticketType;
-    }
+    public String getTicketType(){ return this.ticketType; }
 
-    public void setTicketPrice(int ticketPrice) {
-        this.ticketPrice = ticketPrice;
-    }
+    public int getTicketPrice(){ return this.ticketPrice; }
 
-    public void setTicketStatus(StatusType ticketStatus) {
-        this.ticketStatus = ticketStatus;
-    }
+    public StatusType getTicketStatus(){ return this.ticketStatus; }
 
     @Override
     public String toString(){
